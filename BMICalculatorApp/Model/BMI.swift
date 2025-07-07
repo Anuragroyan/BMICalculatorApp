606a44a5c61ce79ba4e4e9f1330f8f526b0035dd
Anuragroyan <7619354965anurag@gmail.com>
2025-07-07 19:50:21 +0530

    my first initial commit for this app

---
+++ BMICalculatorApp/Model/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMICalculatorApp
+//
+//  Created by Dungeon_master on 31/05/25.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
---
+++ BMICalculatorAppTests/BMICalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorAppTests.swift
+//  BMICalculatorAppTests
+//
+//  Created by Dungeon_master on 31/05/25.
+//
+
+import Testing
+@testable import BMICalculatorApp
+
+struct BMICalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
